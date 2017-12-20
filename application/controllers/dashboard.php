da3da71ec5be83d6af4f9e83b9d244fef6e922c7
embowth <embowth@gmail.com>
2017-12-20 13:09:11 +0700

    dashboaed

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -4,18 +4,11 @@ class Dashboard extends CI_Controller {
 	{
 		$this->load->view('view_dashboard');
 	}
-<<<<<<< HEAD
-} //test
-
-
-
-
-=======
 
 	function edit(){
 		
 	}
 }
->>>>>>> ffb18e56b4592fd9d4360304f61ebb4aee1bc702
+
 
 ?>
