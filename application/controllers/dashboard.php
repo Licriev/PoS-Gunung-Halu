d28cdeacb569a088daca60c9297aad636968831f
embowth <embowth@gmail.com>
2017-12-20 19:00:44 +0700

    Add css and create main view

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,16 +1,26 @@
 <?php
 class Dashboard extends CI_Controller {
-	function index()
-	{
-		$this->load->view('view_dashboard');
-	}
 
-	function edit(){
-		//test git lagi	
-	}
-
-	function view(){
+	public function __construct(){
+		parent::__construct();
 		
+	}
+
+
+	public function index(){
+
+		$data = array();
+		$data['sub_view'] = 'view_dashboard';
+
+		$this->load->view('main_view',$data, FALSE);
+	}
+
+	public function test_page(){
+
+		$data = array();
+		$data['sub_view'] = 'view_test_page';
+
+		$this->load->view('main_view', $data, FALSE);
 	}
 }
 
---
+++ application/views/view_components/sidebar_menu.php
@@ -0,0 +1,18 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+  <div class="menu_section">
+    <h3>General</h3>
+    <ul class="nav side-menu" style="">
+      <li class=""><a href="<?php echo base_url();?>"><i class="fa fa-home"></i> Home</a></li>
+
+      <li class=""><a><i class="fa fa-list"></i> Menu <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu" style="display: none;">
+          <li><a href="<?php echo base_url('dashboard/test_page');?>">Test Page</a></li>
+          <li><a href="#">Menu 2</a></li>
+          <li><a href="#">Menu 3</a></li>
+        </ul>
+      </li>
+
+    </ul>         
+  </div>
+
+</div>
