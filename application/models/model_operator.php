2605f362d046593641a269d1816904e938fc81a1
aji setiawan <ajiunsada@gmail.com>
2017-12-21 09:53:25 +0700

    revisi

--- application/models/model_operator.php
+++ application/models/model_operator.php
@@ -3,7 +3,7 @@ class model_operator extends CI_Model {
 	function login($username,$password)
 	{
 		$cek = $this->db->get_where('operator',array('username'=>$username,'password'=>md5($password)));
-		if($cek->cubrid_num_rows()>0)
+		if($cek->num_rows()>0)
 		{
 			return 1;
 		}
